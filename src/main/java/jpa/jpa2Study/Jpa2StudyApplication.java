f933a9bf79921dce6f84422fa95610d079831d5d
최영찬 <gks54410@gmail.com>
2022-03-01 15:48:24 +0900

    1. 프로젝트 설정

---
+++ src/main/java/jpa/jpa2Study/Jpa2StudyApplication.java
@@ -0,0 +1,13 @@
+package jpa.jpa2Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa2StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa2StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa/jpa2Study/Jpa2StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.jpa2Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa2StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
