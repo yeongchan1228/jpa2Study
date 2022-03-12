0a93e8ca9e15bc2dde4047d6bbb0c8f681ba2750
최영찬 <gks54410@gmail.com>
2022-03-13 00:24:40 +0900

    16. JpaShop OrderRepository 안 api 메소드 삭제

--- src/main/java/jpa/jpa2Study/jpashop/repository/OrderRepository.java
+++ src/main/java/jpa/jpa2Study/jpashop/repository/OrderRepository.java
@@ -77,13 +77,13 @@ public class OrderRepository {
         return resultList;
     }
 
-    public List<OrderSimpleQueryDto> findOrderDtos() {
-        return em.createQuery("select new jpa.jpa2Study.jpashop.repository.OrderSimpleQueryDto(" +
+    /*public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery("select new jpa.jpa2Study.jpashop.repository.dto.OrderSimpleQueryDto(" +
                         "o.id, m.name, o.orderDate, o.status, d.address)" +
                         " from Order o " +
                         "join o.member m" +
                         " join o.delivery d",
                 OrderSimpleQueryDto.class).getResultList();
 
-    }
+    }*/
 }
